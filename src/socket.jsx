baacb1bcbbc9eb50e4e60ad0457e0d2aa51aab3e
pankajpvx <pankajdev627@gmail.com>
2024-09-25 01:24:37 +0530

    fix socket url

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useMemo } from "react";
 import { io } from "socket.io-client";
+import { server } from "./constants/config";
 
 const SocketContext = createContext();
 const getSocket = () => useContext(SocketContext);
@@ -7,7 +8,7 @@ const getSocket = () => useContext(SocketContext);
 const SocketProvider = ({ children }) => {
   const socket = useMemo(
     () =>
-      io("http://localhost:3000", {
+      io(server, {
         withCredentials: true,
       }),
     []
